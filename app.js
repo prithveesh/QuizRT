2d997b08d1b9ea77795aa0d128d17ff0fbd0d467
GS <gsingh9@sapient.com>
2018-01-17 15:06:45 +0530

    Added swagger

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const bodyParser = require('body-parser');
 const cors =  require('cors');
 // const mongoConn = require('./mongoConnection')();
 const server = require('express')();
+const swaggerUi = require('swagger-ui-express');
+
 const config = require('./config.js');
+const swaggerDocument = require('./swagger.json');
 
 // Setup bodyParsing middleware
 server.use(bodyParser.json());
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.listen(config.SERVER_URI, () => {
 	console.log(`server listen at ${config.SERVER_URI}`);
